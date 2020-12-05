767faae477e0843d9274ebd9312cc67a72db70f0
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-12-05 14:26:17 -0600

    Refactor: Move block into new function.

--- config_decorator/config_decorator.py
+++ config_decorator/config_decorator.py
@@ -1016,16 +1016,19 @@ def section(cls_or_name, parent=None):
 
         return cfg_dcor
 
-    # Check if decorator was @passive or @emphatic().
-    if inspect.isclass(cls_or_name):
-        # The decorator was used without being invoked first, e.g.,
-        #   @section
-        #   class Classy...
-        _add_section(cls_or_name)
-        return cls_or_name
-    else:
-        # The decorator was invoked first with arguments, so return the
-        # actual decorator which Python will call back immediately with
-        # the class being decorated.
-        return _add_section
+    def _section():
+        # Check if decorator was @passive or @emphatic().
+        if inspect.isclass(cls_or_name):
+            # The decorator was used without being invoked first, e.g.,
+            #   @section
+            #   class Classy...
+            _add_section(cls_or_name)
+            return cls_or_name
+        else:
+            # The decorator was invoked first with arguments, so return the
+            # actual decorator which Python will call back immediately with
+            # the class being decorated.
+            return _add_section
+
+    return _section()
 
